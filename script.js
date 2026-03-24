b7c3c0e7dac671073cfae5c056cf3f312cac6e99
darkg6421-tech <darkg6421@gmail.com>
2026-03-24 11:05:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -87,10 +87,10 @@ if (count < 3) {
     <p style="color:#38bdf8; font-size:12px;">🔥 Important</p>
   ` + div.innerHTML;
 
-  count++;
 }
       if (count >= 10) return;
   container.appendChild(div);
+  count++;
     });
     if (container.innerHTML === "") {
   container.innerHTML = "⚠️ No news found in this category";
